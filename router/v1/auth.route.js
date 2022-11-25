b0858a6190decd62ce7186d1e5d59e13d3571556
Alexander Ayra <alexander.ayras@gmail.com>
2022-11-25 00:15:45 -0500

    Controller del Auth y generar JWT

--- router/v1/auth.route.js
+++ router/v1/auth.route.js
@@ -1,8 +1,18 @@
-const { Router } = require('express');
+const {Router} = require("express");
+const {check} = require("express-validator");
+const {login} = require("../../controller/auth");
+const {validarCampos} = require("../../middlewares/validar-Campos");
 
 const router = Router();
 
+router.post(
+    "/login",
+    [
+        check("correo", "El correo es Obligatorio").isEmail(),
+        check("contraseña", "La contraseña es Obligatoria").not().isEmpty(),
+        validarCampos,
+    ],
+    login
+);
 
-
-
-module.exports = router
+module.exports = router;
